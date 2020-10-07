c98604ecd08dfa69e0ec41997bb592f50fc01101
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-07 22:16:54 +0300

    sm6250-common: properly add hardware/google/interfaces to soong namespaces
    * to build pixel-power-ext-V1-ndk_platform.so from source

--- common.mk
+++ common.mk
@@ -232,6 +232,7 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
     hardware/google/pixel \
     hardware/xiaomi \
     vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
